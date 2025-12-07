e1e38d2ebf0299cb93336379342982c3101781c9
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-12-07 20:26:34 +0530

    valid token

--- backend/middleware/fetchUser.js
+++ backend/middleware/fetchUser.js
@@ -1,24 +1,23 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const fetchUser = (req, res, next) => {
-  
-    const token = req.header('auth-token');
-    
-    if (!token) {
-        return res.status(401).send({ error: "Please authenticate using a valid token" });
-    }
+  const token = req.header("auth-token");
 
-    try {
-        
-        const data = jwt.verify(token, process.env.JWT_SECRET);
-        
-       
-        req.user = data; 
-        
-        next(); 
-    } catch (error) {
-        res.status(401).send({ error: "Please authenticate using a valid token" });
-    }
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
+  }
+
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = data;
+
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Please authenticate using a valid token" });
+  }
 };
 
 module.exports = fetchUser;
